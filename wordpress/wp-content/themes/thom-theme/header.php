eb0696201bd036cde37640cb41b6ac32a6dcb7ea
Thom Wise <thom.gkw.wp@gmail.com>
2020-01-27 16:52:08 -0800

    Set up blank portfolio

---
+++ wordpress/wp-content/themes/thom-theme/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+		<?php wp_head(); ?>
+		<script>
+			// conditionizr.com
+			// configure environment tests
+			conditionizr.config({
+					assets: '<?php echo get_template_directory_uri(); ?>',
+					tests: {}
+			});
+		</script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="wrapper">
+
+			<!-- HEADER STARTS -->
+			<header id="scroller" class="navbar navbar-default navbar-fixed-top">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a id="brand" class="navbar-brand" href="#">>Thomas Wise<</a>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a id="deco" href="#home">Home</a></li>
+							<li><a id="deco" href="#about">About</a></li>
+							<li><a id="deco" href="#contact">Contact</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</header>
+			<!-- HEADER ENDS -->
---
+++ wordpress/wp-content/themes/thom-theme/index.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<!-- HERO STARTS -->
+			<div id="home" class="jumbotron no-space">
+				<div class="row">
+					<div class="col-xs-12">
+						<h1 class="heading pull-right right-space no-pad">>Web Developer<</h1>
+						<img src="img/thomwise.jpg" class="self img-responsive center-block" alt="Self Portrait">
+					</div>
+				</div>
+			</div>
+			<!-- HERO ENDS -->
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
